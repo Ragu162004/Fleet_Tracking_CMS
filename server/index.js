c93fa5acb114413dd96818f091b80d91127200bc
RAMACHANDRAN A <125943533+rama563@users.noreply.github.com>
2025-04-29 15:52:56 +0530

    added parser

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+
 app.listen(port, () => {
-    console.log(`Server listening on the port: ${port}`);
+  console.log(`Server listening on the port: ${port}`);
 });
