723c7121142203361fd48c3e5e927740ba8a7835
PRIYASAKI <125540653+PRIYASAKI@users.noreply.github.com>
2025-05-05 15:27:34 +0530

    Added footer

--- client/src/components/common/footer.jsx
+++ client/src/components/common/footer.jsx
@@ -1,32 +1,28 @@
-import React from 'react'
-//import './footer.css'
+import React from 'react';
 
-function footer() {
+function Footer() {
   return (
-    
-  <footer className="bg-black text-white p-4 d-flex justify-space-between font-size-14 font-family: Arial mb-0 position-relative">
-  <div className="w-0.3">
-    <h3 className="mb-0.3 text-amber-500">About Us</h3>
-    <p>Welcome to our Fleet Tracking System</p>
-
-  </div>
-  <div className="w-0.3">
-    <h3 className="mb-0.1 text-amber-500 ">Quick Links</h3>
-    <ul className="p-4">
-      <li><a href="#">Home</a></li>
-      <li><a href="#">Track</a></li>
-      <li><a href="#">Contact</a></li>
-    </ul>
-  </div>
-  <div className="w-0.3">
-    <h3 className="mb-0.1 text-amber-500">Contact</h3>
-    <p>Email: abc@example.com</p>
-    <p>Phone: +123 456 7890</p>
-  </div>
-</footer>
-
-
-    
-  )
+    <footer className="bg-blue-800 text-white p-4 font-size-14 font-[Arial] justify-around flex  flex-wrap ">
+      <div className="mb-6">
+        <h3 className="mb-3 text-amber-500">About Us</h3>
+        <p className="mb-3">Welcome to our Fleet Tracking System.</p><br />
+        <p></p>
+      </div>
+      <div className="mb-6">
+        <h3 className="mb-3 text-amber-500">Quick Links</h3>
+        <ul className="p-0">
+          <li className="mb-2"><a href="#" className='text-white-300 hover:bg-amber-400'>Home</a></li>
+          <li className="mb-2"><a href="#" className='text-white-300 hover:bg-amber-400'>Track</a></li>
+          <li className="mb-2"><a href="#" className='text-white-300 hover:bg-amber-400'>Contact</a></li>
+        </ul>
+      </div>
+      <div>
+        <h3 className="mb-3 text-amber-500">Contact</h3>
+        <p className="mb-3">Email: abc@gmail.com</p>
+        <p className="mb-3">Phone: +123 456 7890</p>
+      </div>
+    </footer>
+  );
 }
-export default footer;
+
+export default Footer;
