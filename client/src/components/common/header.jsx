bd9e1246007bc86676aac49e1051fd058be915af
Ragu162004 <ragu16102004@gmail.com>
2025-05-05 14:49:45 +0530

    updated header

--- client/src/components/common/header.jsx
+++ client/src/components/common/header.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
 import { Truck } from "lucide-react"
+import { Link } from 'react-router-dom'
 
 
 const Header = () => {
     return (
-        <div className=''>
-            <div>
-                <p>Logo</p>
-                <Truck size={60} />
+        <div className='flex items-center justify-around p-4 text-white bg-blue-800 capitalize'>
+            <div className='flex justify-center items-center gap-2'>
+                <Truck size={50} />
+                <p className='text-2xl'></p>
             </div>
-            <ul>
-                <li className="item">home</li>
-                <li className="item">track vehicle</li>
-                <li className="item">about</li>
-                <li className="item">contact us</li>
+            <ul className='flex gap-8'>
+                <li>home</li>
+                <li>track</li>
+                <li>about</li>
+                <li>contact us</li>
             </ul>
+            <Link className='px-4 py-2 outline-2 hover:bg-fuchsia-50 hover:text-black rounded-[10px]'>Login</Link>
         </div>
     )
 }
